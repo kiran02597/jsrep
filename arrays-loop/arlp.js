106e6ef360394fd838f28a1fc69eaf97ee23380a
kiran <kskirankumar969@gmail.com>
2021-08-12 22:02:31 +0530

    "updated

--- arrays-loop/arlp.js
+++ arrays-loop/arlp.js
@@ -6,3 +6,13 @@ states.pop();
 console.log(states);
 var name = "kiran";
 console.log(Array.from(name));
+//call back and array functions
+
+function isEven(element) {
+    if(element %2===0) {
+        return true
+    }
+    return false
+}
+
+console.log(isEven(3));
