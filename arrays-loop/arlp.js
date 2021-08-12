8d889201dd60930fd2d202501a734c3d60e9813b
kiran <kskirankumar969@gmail.com>
2021-08-12 20:35:19 +0530

    added file for array tr

---
+++ arrays-loop/arlp.js
@@ -0,0 +1,8 @@
+var countries = ["india","america"];
+var states = new Array("ts","kar");
+console.log(states);
+console.log(states.length);
+states.pop();
+console.log(states);
+var name = "kiran";
+console.log(Array.from(name));
