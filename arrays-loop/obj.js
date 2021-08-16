9df27ae0e1c4964a975c0009343cb37ad1bb7f08
kiran <kskirankumar969@gmail.com>
2021-08-16 18:29:20 +0530

    added object

---
+++ arrays-loop/obj.js
@@ -0,0 +1,26 @@
+var user = {
+    fname : "sai",
+    lname:"keshetti",
+    count :32,
+    loggedin: true,
+    couselist :[],
+    buycoursename : function (coursename) {
+        this.couselist.push(coursename)
+
+    },
+    getcoursecount : function (){
+        return `${this.fname} has ${this.couselist.length} courses`
+    },
+    getinfo : function() {
+        console.log(`logging ${this.fname} info`);
+        console.table(this)
+    }
+
+};
+console.log(user.lname);
+console.log(user["loggedin"]);
+user.count=44;
+console.log(user.buycoursename("JS"));
+console.log(user.getcoursecount());
+//console.table(user);
+console.log(user.getinfo());
